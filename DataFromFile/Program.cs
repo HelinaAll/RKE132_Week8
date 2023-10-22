c5daa6899a67d8d9e3a22b4d57120e4346ebfab2
Helina Allese <helinaallese@Helinas-MacBook-Air.local>
2023-10-22 13:15:55 +0300

    add week8 code

---
+++ DataFromFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"/Users/helinaallese/projects/heroes.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            foreach(string element in dataFromFile)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"/Users//projects/villains.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            foreach (string element in dataFromFile)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ DataFromFileVillains/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"/Users//projects/villains.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            foreach (string element in dataFromFile)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
